83b56a278e7132a12d931aac42f61204c8b30dd5
Dinesh T N <ursdinesh2008@gmail.com>
2026-08-01 09:06:12 +0530

    Fix class declaration in C_not_all_covered.java

--- C_not_all_covered.java
+++ C_not_all_covered.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main{
+public class C_not_all_covered{
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int N = sc.nextInt();
